4bfb98445c6e7a1faf9e81b16f8372d5078f8b32
Rick <rick4470@gmail.com>
2015-02-14 00:19:36 -0700

    added script for payments

---
+++ ProccessPayment.php
@@ -0,0 +1,114 @@
+<?php
+
+$SECRETEKEY = 'MY SECRETE KEY';
+$DOMAIN = 'domain.com';
+$FILELOCATION = '/location/of/file';
+
+if ($_GET["payment"] !== $SECRETEKEY) {
+  header("Location: $DOMAIN");
+}else{
+  if ($_GET["payment"] == $SECRETEKEY) {
+    if (get_option($_SERVER["REMOTE_ADDR"]) == 1) {
+        header("Location: $DOMAIN");
+    }else{
+        update_option($_SERVER["REMOTE_ADDR"], true);
+        header("Content-Type: application/zip");
+        header("Content-Length: " . filesize($FILELOCATION));
+        readfile($FILELOCATION);
+        exit;
+    }
+  }
+}
+
+function update_option($name, $value)
+{
+    if(file_exists("../blocked.csv"))
+    {
+        $file = fopen("../blocked.csv","r");
+
+        $exists = false;
+        $csv = "";
+
+        while(!feof($file))
+        {
+            $data_array = fgetcsv($file);
+
+            if($data_array[0] == $name)
+            {
+                $exists = true;
+            }
+            else
+            {
+                $csv = $csv . $data_array[0] . "," . $data_array[1] . "\n";
+            }
+        }
+
+        fclose($file);
+
+        $csv = substr($csv, 0, -2);
+
+        if($exists)
+        {
+            $csv = $csv . $name . "," . $value . "\n";
+            file_put_contents("../blocked.csv", $csv);
+        }
+        else
+        {
+            $csv = file_get_contents("../blocked.csv") . $name . "," . $value . "\n";
+            file_put_contents("../blocked.csv", $csv); 
+        }
+    }
+    else
+    {
+        $csv = $name . "," . $value . "\n";
+        file_put_contents("../blocked.csv", $csv);
+    }
+}
+
+function get_option($name)
+{
+    $file = fopen("../blocked.csv","r");
+
+    while(!feof($file))
+    {
+        $data_array = fgetcsv($file);
+
+        if($data_array[0] == $name)
+        {
+            fclose($file);
+            return $data_array[1];
+        }
+        
+    }
+
+    fclose($file);
+    return null;
+}
+
+function delete_option($name)
+{
+    if(file_exists("../blocked.csv"))
+    {
+        $file = fopen("../blocked.csv","r");
+        $csv = "";
+
+        while(!feof($file))
+        {
+            $data_array = fgetcsv($file);
+
+            if($data_array[0] == $name)
+            {}
+            else
+            {
+                $csv = $csv . $data_array[0] . "," . $data_array[1] . "\n";
+            }
+        }
+
+        fclose($file);
+
+        $csv = substr($csv, 0, -2);
+
+        file_put_contents("../blocked.csv", $csv);
+    }
+}
+?>
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# EbookSeller
-Block list of users after they purchase and item from you.
+## PHP Script to Serve Files and then Block users
+
+This script was used for [LearnMean.com](http://learnmean.com/#!/) to proccess payments from paypal then serve a zip folder to the user, and then add them to the blocked.csv file so they wont be able to download the file more then once.
+
+The `$SECRETEKEY` is stored when you create a [paypal button](https://www.paypal.com/us/cgi-bin/webscr?cmd=_singleitem-intro-outside)
+
+The `$FILELOCATION` is where the file you want to serve is located on the server.
+
+If you want to use pretty urls configure your server to use the `.htaccess` provided. 
